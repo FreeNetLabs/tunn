ff6b009130ea2e0c166dfb77a560ef16d63255e5
Ayan Rajpoot <ayanrajpoot2004@gmail.com>
2026-03-22 00:13:44 +0530

    refactor: remove config command and integrate configuration loading in main function

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/FreeNetLabs/tunn/cmd"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/FreeNetLabs/tunn/internal/tunnel"
+	"github.com/FreeNetLabs/tunn/pkg/config"
+)
 
 func main() {
-	cmd.Execute()
+	configFile := flag.String("config", "config.json", "config file path")
+	flag.StringVar(configFile, "c", "config.json", "config file path (shorthand)")
+	flag.Parse()
+
+	cfg, err := config.LoadConfig(*configFile)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Printf("Mode: %s\n\n", cfg.Mode)
+
+	manager := tunnel.NewManager(cfg)
+	if err := manager.Start(); err != nil {
+		log.Fatalf("failed to start tunnel: %v", err)
+	}
 }
